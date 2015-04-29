47935bcae8abf4dcf13e122d5ba84cd8f7035b3f
allencloud <allen.sun@daocloud.io>
2015-04-29 15:06:01 +0800

    init commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ubuntu:14.04
+
+MAINTAINER Allen Sun allen.sun@daocloud.io
+
+RUN sed -i "s/archive\.ubuntu\.com/mirrors\.163\.com/g" /etc/apt/sources.list \
+        && apt-get update \
+        && apt-get install -y python \
+        && apt-get install -y python-pip \
+        && pip install -i http://pypi.douban.com/simple requests 
+        && apt-get clean
+
+COPY ./sine.py /sine.py
+
+CMD ["python", "/sine.py"]
---
+++ sine.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import json
+import math
+import requests
+import sys
+import os
+
+from time import sleep
+
+# Read the environment value
+host = os.getenv('INFLUXDB_PORT_8086_TCP_ADDR')
+if host is None:
+    host = "localhost"
+
+port = os.getenv('INFLUXDB_PORT_8086_TCP_PORT')
+if port is None:
+    port = '8083'
+
+user = os.getenv('INFLUXDB_USERNAME')
+if user is None:
+    user = 'root'
+
+password = os.getenv('INFLUXDB_PASSWORD')
+if password is None:
+    password = 'root'
+
+
+STATUS_MOD = 10
+n = 0
+
+# Firstly use the credentials to create an influxdb database.
+# We take name db1 as an instance.
+db = 'db1'
+
+url = 'http://%s:%s/db'%( host , port )
+params={ 'u' : user , 'p' : password }
+data = { 'name' : db }
+
+# Start to create influxdb database
+r = requests.post( url, params=params, data=data)
+print r.json()
+
+
+# Start to to generate points and draw them into influxdb
+while True:
+    for d in range(0, 360):
+        v = [{'name': 'sin', 'columns': ['val'], 'points': [[math.sin(math.radians(d))]]}]
+        url = 'http://%s:%s/db/%s/series?u=%s&p=%s'%(host,port,db,user,password)
+        print url
+        r = requests.post(url, data=json.dumps(v))
+        if r.status_code != 200:
+            print 'Failed to add point to influxdb -- aborting.'
+            sys.exit(1)
+        n += 1
+
+        sleep(0.01)
+
+        if n % STATUS_MOD == 0:
+            print '%d points inserted.' % n
